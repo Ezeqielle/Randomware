bd3975c6cdb9e95fbc8743c8553c57f9011ad3fe
Peter Balivet <peterbalivet@gmail.com>
2021-01-24 20:21:28 +0100

    added package keys + added public/private file encryption

---
+++ encryption/keys/keys.go
@@ -0,0 +1,16 @@
+package keys
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"log"
+)
+
+// GenerateKeyPair : generates a new key pair
+func GenerateKeyPair(bits int) (*rsa.PrivateKey, *rsa.PublicKey) {
+	privkey, err := rsa.GenerateKey(rand.Reader, bits)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return privkey, &privkey.PublicKey
+}
---
+++ encryption/keys/public.go
@@ -0,0 +1,58 @@
+package keys
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/sha512"
+	"crypto/x509"
+	"encoding/pem"
+	"log"
+)
+
+// PublicKeyToBytes : public key to bytes
+func PublicKeyToBytes(pub *rsa.PublicKey) []byte {
+	pubASN1, err := x509.MarshalPKIXPublicKey(pub)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pubBytes := pem.EncodeToMemory(&pem.Block{
+		Type:  "RSA PUBLIC KEY",
+		Bytes: pubASN1,
+	})
+
+	return pubBytes
+}
+
+// BytesToPublicKey : bytes to public key
+func BytesToPublicKey(pub []byte) *rsa.PublicKey {
+	block, _ := pem.Decode(pub)
+	enc := x509.IsEncryptedPEMBlock(block)
+	b := block.Bytes
+	var err error
+	if enc {
+		b, err = x509.DecryptPEMBlock(block, nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	ifc, err := x509.ParsePKIXPublicKey(b)
+	if err != nil {
+		log.Fatal(err)
+	}
+	key, ok := ifc.(*rsa.PublicKey)
+	if !ok {
+		log.Fatal("Bad public key")
+	}
+	return key
+}
+
+// EncryptWithPublicKey : encrypts data with public key
+func EncryptWithPublicKey(msg []byte, pub *rsa.PublicKey) []byte {
+	hash := sha512.New()
+	ciphertext, err := rsa.EncryptOAEP(hash, rand.Reader, pub, msg, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return ciphertext
+}
